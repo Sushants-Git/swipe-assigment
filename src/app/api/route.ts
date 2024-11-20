6b4f7e721c1d33f1b8e2bee87bb12541bdf56f89
Sushants-Git <sushantsgml@gmail.com>
2024-11-20 09:41:52 +0530

    build fix : unused variables

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,15 +1,16 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextResponse } from "next/server";
 
-export async function POST(req, res) {
-    console.log("reached");
-    const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+export async function POST() {
+    if (process.env.GOOGLE_API_KEY) {
+        const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-    const prompt = "Write a story about a magic backpack.";
+        const prompt = "Write a story about a magic backpack.";
 
-    const result = await model.generateContent(prompt);
-    return NextResponse.json({
-        output: result.response.text(),
-    });
+        const result = await model.generateContent(prompt);
+        return NextResponse.json({
+            output: result.response.text(),
+        });
+    }
 }
